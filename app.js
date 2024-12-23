ffb1e040a0d7b7c995d8d64b65310b8b75ae80cb
Mukhammadrasul2011 <msamaritdinov@gmail.com>
2024-12-23 11:28:22 +0500

    changed

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
 
 const PORT = 8080;
 
-app.get('/home/blog', (req, res) => {
-    try {
-        res.status(200).send('Hello world');
-    } catch (error) {
-        console.log(error);
-    }
+app.get("/home/blog", (req, res) => {
+  try {
+    res.status(200).send("Hello world");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.post('/api/post', (req, res) => {
-    try{
-        const { firstName, lastName, email } = req.body;
+const DB_URL =
+  "mongodb+srv://Muhammadrasul:smb1102.m@cluster0.8ho8t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-        res.send(`My full Name is ${firstName} ${lastName}`);
-    } catch (error) {
-        res.send(`Error this is Post req -- $(error)`);
-    }
-})
+const connectBDFunc = async () => {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => {
+      console.log(`Listen to -- https://localhost:${PORT}`);
+    });
+    console.log("Connect to DB");
+  } catch (error) {
+    console.log(`Error in connect to DB -- ${error}`);
+  }
+};
+connectBDFunc();
 
-app.listen(PORT, () => console.log(`Listen to -- https://localhost:${PORT}`));
+app.post("/api/post", (req, res) => {
+  try {
+    const { firstName, lastName, email } = req.body;
+
+    res.send(`My full Name is ${firstName} ${lastName}`);
+  } catch (error) {
+    res.send(`Error this is Post req -- $(error)`);
+  }
+});
